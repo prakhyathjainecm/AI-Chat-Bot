5a04f9be08db1486dde63baa9555ae7fbc7b332e
prakhyathjainecm <133338859+prakhyathjainecm@users.noreply.github.com>
2023-05-12 13:54:13 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,44 @@
+import openai
+import gradio as gr
+
+openai.api_key = "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"
+
+messages = [
+    {
+        "role": "system",
+        "content": "You are a helpful and kind AI Assistant."
+    },
+]
+
+def chatbot(input):
+    if input:
+        messages.append(
+            {
+                "role": "user", 
+                "content": input
+            }
+        )
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", 
+            messages=messages
+        )
+        reply = chat.choices[0].message.content
+        messages.append(
+            {
+                "role": "assistant", 
+                "content": reply
+            }
+        )
+        return reply
+
+inputs = gr.inputs.Textbox(lines=7, label="Chat with AI")
+outputs = gr.outputs.Textbox(label="Reply")
+
+gr.Interface(
+    fn=chatbot, 
+    inputs=inputs, 
+    outputs=outputs, 
+    title="AI Chatbot",
+    description="Ask anything you want",
+    theme="compact"
+).launch(share=True)
